3edd1afd5223d2ff16085db139e89bea57202aec
Favio Tejada <favio_tejada@hotmail.com>
2020-05-03 20:45:19 +0200

    Fix el missing dependency for tomcat 8

--- src/main/java/com/example/demojsfspringboot/model/Flight.java
+++ src/main/java/com/example/demojsfspringboot/model/Flight.java
@@ -6,12 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 @Entity
 public class Flight {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY) 
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String airline;
